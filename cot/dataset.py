1af7ea4b8050dae0afdbb649a37dd99da39637dd
Kieron Kretschmar <k.kretschmar@hotmail.de>
2023-05-12 12:19:29 +0200

    WIP on dataset

--- cot/dataset.py
+++ cot/dataset.py
@@ -1,61 +1,94 @@
-from model_utils import load_model_dicts, load_model
-from datasets import load_from_disk
 import os
-from datasets import load_dataset
+import torch
+from datasets import load_from_disk
+from datasets import get_dataset_config_names, load_dataset
+from transformers import AutoTokenizer
 
-# adapted from https://github.com/philschmid/deep-learning-pytorch-huggingface/blob/main/training/peft-flan-t5-int8-summarization.ipynb
 
+# SETUP
+# os.system('pip install git+https://github.com/google/BIG-bench.git')
+# os.system('pip install datasets')
 
-def prep_data(tokenizer, data_dir = '/project/gpuuva021/shared/cot/data/test', hf_cache = '/project/gpuuva021/shared/cot/hf_cache'):
+config = {
+    "raw_data_path": None,
+    "cot_path": None,
+    "preprocessed_dir": None,
+    "bigbench_task_name": None,
+    "bigbench_explanations_directory": None,
+    "tokenizer_name": None,
+
+    "rebuild_cache": False,
+
+    "n_shot": 0,
+
+}
+
+class CoTDataset(torch.utils.data.Dataset):
+    def __init__(self, config):
+        self.config = config
+
+        self.tokenizer = None
+
+        # If necessary, download, tokenize dataset and save to disk
+        preprocessed_path = self.config["preprocessed_dir"] + "/" + self.preprocessed_filename()
+        if config["rebuild_cache"] or os.path.exists(preprocessed_path):
+            # TODO: Look at notebook
+            # https://colab.research.google.com/drive/1MKdLdF7oqrSQCeavAcsEnPdI85kD0LzU?usp=sharing#scrollTo=UqoBR0ujfEkl
+
+            dt = load_dataset('bigbench', self.config["bigbench_task_name"])
+            tokenized_dataset = {}
+            for split in dt:
+                tokenized_dataset[split] = {}
+                if split=="default" and "train" in dt:
+                    continue
+                
+                # Tokenize
+                # (Some tasks might require task-dependent formatting)
+                tokenized_dataset[split]["inputs"] = [self.tokenizer(sample["inputs"]) for sample in dt[split]]
+                tokenized_dataset[split]["targets"] = [self.tokenizer(sample["targets"]) for sample in dt[split]]
+                
+                # Save to disk
+                # TODO
+
     
 
-    # Load dataset from the hub
-    dataset = load_dataset('samsum', cache_dir=hf_cache)
+        # Load dataset from disk
 
-    print(f"Train dataset size: {len(dataset['train'])}")
-    print(f"Test dataset size: {len(dataset['test'])}")
+        # Tokenize cot's
 
-    from datasets import concatenate_datasets
-    import numpy as np
-    # The maximum total input sequence length after tokenization. 
-    # Sequences longer than this will be truncated, sequences shorter will be padded.
-    tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["dialogue"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-    input_lenghts = [len(x) for x in tokenized_inputs["input_ids"]]
-    # take 85 percentile of max length for better utilization
-    max_source_length = int(np.percentile(input_lenghts, 85))
-    print(f"Max source length: {max_source_length}")
+        # Store cot's
+        self.cots = [{
+            "id": None,
+            "tokenized_explanation" : None,
+            "explanation": None,
+            "label": None,
+        }]
 
-    # The maximum total sequence length for target text after tokenization. 
-    # Sequences longer than this will be truncated, sequences shorter will be padded."
-    tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["summary"], truncation=True), batched=True, remove_columns=["dialogue", "summary"])
-    target_lenghts = [len(x) for x in tokenized_targets["input_ids"]]
-    # take 90 percentile of max length for better utilization
-    max_target_length = int(np.percentile(target_lenghts, 90))
-    print(f"Max target length: {max_target_length}")
+        pass
 
-    def preprocess_function(sample,padding="max_length"):
-        # add prefix to the input for t5
-        inputs = ["summarize: " + item for item in sample["dialogue"]]
+    def preprocessed_filename(self):
+        return ""
 
-        # tokenize inputs
-        model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+    def __getitem__(self, idx):
+        # Select CoT's (includes shuffling if dynamic)
 
-        # Tokenize targets with the `text_target` keyword argument
-        labels = tokenizer(text_target=sample["summary"], max_length=max_target_length, padding=padding, truncation=True)
+        # Concatenate 
 
-        # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-        # padding in the loss.
-        if padding == "max_length":
-            labels["input_ids"] = [
-                [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
-            ]
+        # Return x and y
+        pass
 
-        model_inputs["labels"] = labels["input_ids"]
-        return model_inputs
+    def __len__(self):
+        return len(self.data.labels)
 
-    tokenized_dataset = dataset.map(preprocess_function, batched=True, remove_columns=["dialogue", "summary", "id"])
-    print(f"Keys of tokenized dataset: {list(tokenized_dataset['train'].features)}")
 
-    # save datasets to disk for later easy loading
-    tokenized_dataset["train"].save_to_disk(os.path.join(data_dir, 'train'))
-    tokenized_dataset["test"].save_to_disk(os.path.join(data_dir, 'eval'))
+class CoTDataLoader(torch.utils.data.DataLoader):
+
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def collate(self, batch):
+        pass
+
+
+# Testing
+test_ds = CoTDataset(config)
