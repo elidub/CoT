74831d27c551da73f634c3040495c80f42823293
OreGorgo <62994226+OreGorgo@users.noreply.github.com>
2023-05-21 14:46:22 +0200

    default parameters change

--- cot/train.py
+++ cot/train.py
@@ -24,14 +24,14 @@ def parse_option():
     parser.add_argument('--bigbench_explanations_dataset', default = 'truthful_qa', type=str, help='The name of the bigbench task from which the CoTs are taken.')
     parser.add_argument('--bigbench_explanations_type', default = 'handtuned', type=str, help='Type of explanations from Lampinen et al. One of "handtuned", "selected", "untuned".')
     parser.add_argument('--bigbench_explanations_path', default = 'data/bigbench-explanations/', type=str, help='Path to the bigbench explanations from Lampinen et al.')
-    parser.add_argument('--n_shot', default = 5, type=int, help='How many examples to show in-context')
+    parser.add_argument('--n_shot', default = 3, type=int, help='How many examples to show in-context')
     parser.add_argument('--rebuild_cache', action='store_true', help='Whether to rebuild the cached preprocessed datasets')
     parser.add_argument('--shuffle_cots', default = False, type=bool, help='Whether to randomly select the available CoTs and their order. If False, the first n_shot CoTs are chosen.')
 
     # Training args
     parser.add_argument('--lr', default = 1e-3, type=float, help='Learning rate')
-    parser.add_argument('--max_epochs', default = 100, type=int, help='Maximum number of epochs to train')
-    parser.add_argument('--batch_size', default = 8, type=int, help='Batch size')
+    parser.add_argument('--max_epochs', default = 10, type=int, help='Maximum number of epochs to train')
+    parser.add_argument('--batch_size', default = 32, type=int, help='Batch size')
     parser.add_argument('--seed', default=666, type=int, help="The seed for reproducibility")
 
     # Training args (lora)
