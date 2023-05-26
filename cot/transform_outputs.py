4f9ac85fbc48ebfe9bcfea201bc0aa867dd460fa
Elias Dubbeldam <eliasdubbeldam@gmail.com>
2023-05-26 16:22:32 +0200

    compatible to detect sequences

--- cot/transform_outputs.py
+++ cot/transform_outputs.py
@@ -1,38 +1,34 @@
 import torch
 
-E, A, S, P = 1, 2, 0, -100 # tokens E: explanation, A: answer, S: end of sentence, P: padding
+def transform_outputs(input_tensor: torch.Tensor, A_seq: int, P: int, return_indices = True) -> torch.Tensor:
 
-input_tensor = torch.tensor([ # Example input tokens with 
-    [E, 10, 11, 12, A,  21, 22,  23,  S, P, P],
-    [E, 13, 14, 15, 16, 17, A,   24, 25, S, P],
-    [E, 18, A,  26, 27, 28, S,    P,  P, P, P]
-])
-
-goal_tensor = torch.tensor([
-    [  21,   22,   23,    S, P, P, P, P, P, P, P],
-    [  24,   25,    S,    P, P, P, P, P, P, P, P],
-    [  26,   27,   28,    S, P, P, P, P, P, P, P]
-])
-
-
-def transform_outputs(input_tensor: torch.Tensor, E: int, A: int, S: int, P: int) -> torch.Tensor:
+    B, L = input_tensor.size() # Batch, Length of sentence
 
     assert P < 0
+    assert len(A_seq) == 2
+
+    A0, A1 = A_seq
 
     # Get the mask where the tokens are A
-    mask_A = (input_tensor == A)
+    mask_A0 = (input_tensor == A0)
+    mask_A1 = (input_tensor == A1)
 
-     # Cumulative sum on mask, tokens after the first A will have value 1
-    mask_after_A = (mask_A.cumsum(dim=1) == 1).int()
+    mask_after_A0 = (mask_A0.cumsum(dim=1) == 1).int()
+    mask_after_A1 = (mask_A1.cumsum(dim=1) == 1).int()
+
+    # Cumulative sum on mask, tokens after the first A will have value 1
+    mask_after_A = mask_after_A0 + mask_after_A1
+    # mask_after_A = (mask_A.cumsum(dim=1) == 1).int()
 
     # Finding the index of first non-A token after the first A for each row
-    indices = torch.argmax(mask_after_A, dim=1).unsqueeze(-1)
+    indices = torch.argmax(mask_after_A, dim=1)
 
-    # Not sure if this is necessary, but it will make sure that the indices are not out of bounds ?
-    # index_tensor = indices + torch.arange(input_tensor.size(1)).unsqueeze(0).to(input_tensor.device)
+    # If there are no A_seq token, pad the sentence
+    indices_mask = torch.where(indices == 0, L, indices)
+    indices_ret = torch.where(indices == 0, -1, indices)
 
     # Mask the explanation
-    mask_fill_P = (torch.arange(input_tensor.size(1)).unsqueeze(0).to(input_tensor.device) > indices).expand_as(input_tensor)
+    mask_fill_P = (torch.arange(input_tensor.size(1)).unsqueeze(0).to(input_tensor.device) > indices_mask.unsqueeze(-1)).expand_as(input_tensor)
     output_tensor = torch.where(mask_fill_P, input_tensor, P)
 
     # Where the answer 1, assuming thaat all tokens are >= 0
@@ -51,10 +47,34 @@ def transform_outputs(input_tensor: torch.Tensor, E: int, A: int, S: int, P: int
     # Pad the block with P to original size
     tokens_padded = torch.nn.functional.pad(tokens_block, (0, output_tensor.shape[1] - tokens_block.shape[1]), mode='constant', value=P)
 
-    return tokens_padded
+    return (tokens_padded, indices_ret) if return_indices else tokens_padded
+
+if __name__ == '__main__':
+    # Example
+    E, A_seq, S, P = 1, (2, 3), 0, -100 # tokens E: explanation, A: answer_seq (e.g. <Answer> <colon>), S: end of sentence, P: padding
+
+    input_tensor = torch.tensor([ # Example input tokens with 
+        [E, 10, 11, 12, A_seq[0], A_seq[1], 22,  23,  S, P, P],
+        [E, 13, 14, 15, 16, 17,  A_seq[0], A_seq[1], 25, S, P],
+        [E, 18, A_seq[0], A_seq[1] , 27, 28, 29,    S,  P, P, P],
+        [E, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27],
+        [A_seq[0], A_seq[1], 27, 28, 29, 30, 31, 32, 33, 34, 35],
+        [P,  P, P,  P,  P,  P,  P,  P,  P,  P,  P],
+    ])
+
+    goal_tensor = torch.tensor([
+        [   22,   23,    S, P, P, P, P, P, P, P, P],
+        [   25,    S,    P, P, P, P, P, P, P, P, P],
+        [   27,   28,   29, S, P, P, P, P, P, P, P],
+        [   P,     P,    P, P, P, P, P, P, P, P, P],
+        [   27, 28, 29, 30, 31, 32, 33, 34, 35, P, P],
+        [P,  P, P,  P,  P,  P,  P,  P,  P,  P,  P],
+    ])
 
 
+    tokens_padded, indices = transform_outputs(input_tensor, A_seq, P, return_indices=True)
 
-tokens_padded = process_tensor(input_tensor, E, A, S, P)
+    print('token\n',   tokens_padded)
+    print('indices\n', indices)
 
-assert (tokens_padded == goal_tensor).all()
+    assert (tokens_padded == goal_tensor).all()
